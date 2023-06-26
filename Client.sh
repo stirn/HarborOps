29b8ba453d27b45eb53cf19e047e5029ea63c183
nikolay.labaznikov <nikolay.labaznikov@softswiss.com>
2023-06-27 01:38:30 +0300

    Added GetProjects

--- Client.sh
+++ Client.sh
@@ -28,6 +28,32 @@ GetRepositories() {
     fi
 }
 
+GetProjects() {
+      while getopts "u:k:" opt; do
+        case $opt in
+            u)
+                local api_url="http://"${OPTARG}
+                ;;
+            k)
+                local api_key=$OPTARG
+                ;;
+            *)
+                ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+    response=$(get_projects $api_url $api_key)
+    response_body=$(echo $response | awk -F" :curl_output:" '{print $1}')
+    curl_output=$(echo $response | awk -F" :curl_output:" '{print $2}')
+
+    if [[ $(echo $curl_output) -eq "http_response=200" ]]; then
+            echo $response_body | jq '.[].name'
+        else
+            echo "--- Error: $curl_output" >&2
+            exit 1
+    fi
+}
+
 source $(dirname $(realpath $0))/Menu.sh
 
 #
--- Menu.sh
+++ Menu.sh
@@ -4,6 +4,7 @@ PrintHelp() {
     echo "Usage: $0 <Command> [options]"
     echo "Commands:"
     echo "  GetRepositories                 List all authorized repositories"
+    echo "  GetProjects                     Returns projects created by Harbor"
     echo ""
     echo "Command Options:"
     echo "  Mandatory keys:"
@@ -12,6 +13,10 @@ PrintHelp() {
     echo ""
     echo "  GetRepositories:"
     echo "                          No keys required"
+    echo ""
+    echo "  GetProjects:"
+    echo "                          No keys required"
+    echo ""
     # ...to be continued
 }
 
@@ -19,6 +24,10 @@ case $1 in
     GetRepositories)
         shift
         GetRepositories "$@"
+        ;;
+    GetProjects)
+        shift
+        GetProjects "$@"
         ;;
     *)
         echo "Invalid option: $1"
