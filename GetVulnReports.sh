b1e46e99230419dc8b16eae1251d0b0f9234c4a5
nikolay.labaznikov <nikolay.labaznikov@softswiss.com>
2023-06-27 11:44:22 +0300

    Updated processing

--- GetVulnReports.sh
+++ GetVulnReports.sh
@@ -7,17 +7,23 @@ ReadRepositories () {
     done
 }
 
-
 ReadArtifacts () {
     for r in $repositories; do
         local project=$(echo $r | awk -F"\/" '{print $1}')
         local repository=$(echo $r | awk -F"\/" '{print $2}')
-        echo $(./Client.sh GetArtifacts -u $1 -k $2 -p $project -r $repository | jq '.[].addition_links.vulnerabilities.href')
+        echo $(./Client.sh GetArtifacts -u $1 -k $2 -p $project -r $repository | jq -r '.[].addition_links.vulnerabilities.href')
+    done
+}
+
+ReadVulnReports () {
+    for v in $vuln_reports_links; do
+        echo $(./Client.sh Get -u $1 -k $2 -l $v | jq '.[].generated_at')
     done
 }
 
 projects=$(./Client.sh GetProjects -u $1 -k $2 | jq -r '.[].name')
 repositories=$(ReadRepositories $1 $2)
-vuln_reports=$(ReadArtifacts $1 $2)
+vuln_reports_links=$(ReadArtifacts $1 $2)
+vuln_reports=$(ReadVulnReports $1 $2)
 
 echo $vuln_reports
